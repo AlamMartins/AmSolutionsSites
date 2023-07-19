b419dfbb863d57b70883731a0e87d310eb9efe24
Alam Martins <alammartins@adhaf.haf.com.br>
2023-07-19 11:20:58 -0300

    correção class

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -15,22 +15,22 @@ export default function Testimonials() {
     <h1 className="text-2xl font-bold mb-6 text-center">Registration Form</h1>
     <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md">
       
-    <div class="mb-4">
+    <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
         <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="text" id="name" name="name" placeholder="Digite o seu nome" />
     </div>
-    <div class="mb-4">
+    <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
         {/* <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" id="email" name="email" placeholder="john@example.com"> */}
         <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" id="email" name="email" placeholder="Digite o seu e-mail" />
     </div>
-    <div class="mb-4">
+    <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
         {/* <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" id="email" name="email" placeholder="john@example.com"> */}
         <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" id="email" name="email" placeholder="Digite o seu e-mail" />
     </div>
 
-    <div class="mb-4">
+    <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" for="confirm-password">Confirm Password</label>
         {/* <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password" id="confirm-password" name="confirm-password" placeholder="********"> */}
         {/* <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password" id="confirm-password" name="confirm-password" placeholder="********"> */}
