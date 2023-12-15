b177a7719bd956c5e347017dc9d9cf18d8c18df3
AlamMartins <alammartins@haf.com.br>
2023-12-15 13:45:12 -0300

    centralização hero

--- components/ComponentesTeste/Comp10.jsx
+++ components/ComponentesTeste/Comp10.jsx
@@ -91,7 +91,7 @@ const Comp10 = () => {
     </span>
     {/* SVG Background */}
     {/* Jumbotron */}
-    <div className="  w-[350px] h-[800px] ml-[12px] justify-center -mt-[740px] sm:flex sm:w-auto sm:-mt-[580px] sm:ml-0 " data-aos="zoom-in" data-aos-duration="2000">
+    <div className="  w-[350px] h-[800px] ml-[34px] justify-center -mt-[740px] sm:flex sm:w-auto sm:-mt-[580px] sm:ml-0 " data-aos="zoom-in" data-aos-duration="2000">
       
       <div className="text-center">
         <div
@@ -101,12 +101,12 @@ const Comp10 = () => {
             backdropFilter: "saturate(200%) blur(25px)"
           }}
         >
-          <h1 className="mt-2 mb-12 text-4xl font-bold tracking-tight md:text-6xl xl:text-7xl" data-aos="flip-left" data-aos-duration="2500">
+          <h1 className="mt-2 mb-12 text-4xl font-bold tracking-tight md:text-6xl xl:text-7xl" data-aos="zoom-in" data-aos-duration="2500">
             Criação de Sites Profissionais <br />
             <span className="text-blue-400">para o seu Negócio</span>
           </h1>
           <a
-            className="mb-2 inline-block rounded bg-green-700 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
+            className="mb-2 inline-block rounded bg-[#25D366] px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
             data-te-ripple-init=""
             data-te-ripple-color="light"
             href="#!"
@@ -117,7 +117,7 @@ const Comp10 = () => {
             Solicitar Orçamento
           </a>
           <a
-            className="inline-block rounded bg-gray-500 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:hover:bg-gray-500 dark:hover:bg-opacity-40"
+            className="inline-block rounded bg-[#68686832] px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:hover:bg-gray-500 dark:hover:bg-opacity-40"
             data-te-ripple-init=""
             data-te-ripple-color="light"
             href="#!"
